b00bd6e80e0031cce9a12633ac7f0c17990a7388
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-03-23 13:14:14 +0200

    only 1 card appears in the rv

--- app/src/main/java/com/example/weatherapplication/Model/Minutely.kt
+++ app/src/main/java/com/example/weatherapplication/Model/Minutely.kt
@@ -6,6 +6,8 @@ import com.google.gson.annotations.SerializedName
 data class Minutely (
 
   @SerializedName("dt"            ) var dt            : Int? = null,
-  @SerializedName("precipitation" ) var precipitation : Int? = null
+  //@SerializedName("precipitation" ) var precipitation : Int? = null
+  @SerializedName("precipitation" ) var precipitation : Double? = null
+
 
 )
